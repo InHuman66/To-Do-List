1fddea1af79fa7cc4efeb6a4fbe0e8f54e974c97
InHuman66 <olexandr.kk@gmail.com>
2021-04-18 13:33:30 +0300

    Updates

---
+++ precache-manifest.79f8f5574a7eb8450a7638c7eee626f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41e92e221034c8c083899958162876d",
+    "url": "/To-Do-List/index.html"
+  },
+  {
+    "revision": "a15e44c450de8ca317af",
+    "url": "/To-Do-List/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "c5e6343beeadbe1ee50b",
+    "url": "/To-Do-List/static/js/2.346a37e5.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/To-Do-List/static/js/2.346a37e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a15e44c450de8ca317af",
+    "url": "/To-Do-List/static/js/main.84e1a988.chunk.js"
+  },
+  {
+    "revision": "a75a09460f2a6dbb7116",
+    "url": "/To-Do-List/static/js/runtime-main.4bd6e5d5.js"
+  }
+]);
