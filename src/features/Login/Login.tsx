d929b730b7b269ec58264bb8f220093ac72469a7
InHuman66 <olexandr.kk@gmail.com>
2021-04-18 13:46:42 +0300

    password length removed

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -34,11 +34,6 @@ export const Login = () => {
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
                 errors.email = 'Invalid email address';
             }
-            if (!values.password) {
-                errors.password = 'Required';
-            } else if (values.password.length < 5) {
-                errors.password = 'Invalid password';
-            }
             return errors;
         },
         onSubmit: values => {
